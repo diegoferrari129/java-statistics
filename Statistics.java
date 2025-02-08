757a2745b39de82eaeb531ecc31514ea8714eefe
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2025-02-08 23:53:33 +0100

    [UPD] max and min of "numbers"

--- Statistics.java
+++ Statistics.java
@@ -4,14 +4,26 @@ public class Statistics {
         int[] numbers = { 1, 2, 3, 4, 5 };
 
         int sum = 0;
+        int max = numbers[0];
+        int min = numbers[0];
 
         for (int i = 0; i < numbers.length; i++) {
+
             sum += numbers[i];
+
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
         }
 
         double media = (double) sum / numbers.length;
 
         System.out.println("Sum: " + sum);
         System.out.println("Media: " + media);
+        System.out.println("Max: " + max);
+        System.out.println("Min: " + min);
     }
 }
