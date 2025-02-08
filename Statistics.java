ca1c7ecaa8dc9f5396fb105aea89831e4b49ae9d
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2025-02-08 23:47:40 +0100

    [UPD] media of "numbers"

--- Statistics.java
+++ Statistics.java
@@ -1,11 +1,17 @@
 public class Statistics {
     public static void main(String[] args) {
+
         int[] numbers = { 1, 2, 3, 4, 5 };
 
         int sum = 0;
+
         for (int i = 0; i < numbers.length; i++) {
             sum += numbers[i];
         }
+
+        double media = (double) sum / numbers.length;
+
         System.out.println("Sum: " + sum);
+        System.out.println("Media: " + media);
     }
 }
